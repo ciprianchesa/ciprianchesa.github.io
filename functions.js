30f0a89b3e7ad83c3d728cb56f68abda79080954
chesa_ciprian <chesa_ciprian@yahoo.com>
2021-01-07 20:39:43 +0200

    first version of CV

---
+++ functions.js
@@ -0,0 +1,55 @@
+function showHomePage() {
+    var skills = document.getElementById('skills');
+    skills.style.display = 'none';
+
+    var languages = document.getElementById('languages');
+    languages.style.display = 'none';
+
+    var projects = document.getElementById('projects');
+    projects.style.display = 'none';
+
+    var page = document.getElementById('home');
+    page.style.display = '';
+}
+
+function showSkillsPage() {
+    var home = document.getElementById('home');
+    home.style.display = 'none';
+
+    var languages = document.getElementById('languages');
+    languages.style.display = 'none';
+
+    var projects = document.getElementById('projects');
+    projects.style.display = 'none';
+    
+    var page = document.getElementById('skills'); 
+    page.style.display = '';
+}
+
+function showLanguagesPage() {
+    var home = document.getElementById('home');
+    home.style.display = 'none';
+
+    var skills = document.getElementById('skills');
+    skills.style.display = 'none';
+
+    var projects = document.getElementById('projects');
+    projects.style.display = 'none';
+
+    var page = document.getElementById('languages');
+    page.style.display = '';
+}
+
+function showProjectsPage() {
+    var home = document.getElementById('home');
+    home.style.display = 'none';
+
+    var skills = document.getElementById('skills');
+    skills.style.display = 'none';
+
+    var languages = document.getElementById('languages');
+    languages.style.display = 'none';
+
+    var page = document.getElementById('projects');
+    page.style.display = '';
+}

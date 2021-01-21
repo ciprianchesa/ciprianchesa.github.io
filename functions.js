d0d8022bfe9d423b3d25c40da3992a1935375fc9
Ciprian Chesa <chesa_ciprian@yahoo.com>
2021-01-21 20:08:37 +0200

    add generic showPage and remove specific show*Page

--- functions.js
+++ functions.js
@@ -7,34 +7,16 @@ function show(id) {
 }
 
 function hideAllPages() {
-
     var pages = document.querySelectorAll(".page");
 
-    //initial ; condition   ; post execution
     for(var i = 0; i < pages.length; i++) {
         var page = pages[i];
         hide(page.id);
         // hide(pages[i].id);
-       // hide(pagesIds[i]);
     }
 }
 
-function showHomePage() {
+function showPage(pageId) {
     hideAllPages();
-    show('home');
-}
-
-function showSkillsPage() {
-    hideAllPages();
-    show('skills'); 
-}
-
-function showLanguagesPage() {
-    hideAllPages();
-    show('languages');
-}
-
-function showProjectsPage() {
-    hideAllPages();
-    show('projects');
+    show(pageId);
 }

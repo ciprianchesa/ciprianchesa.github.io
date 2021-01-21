1a83a015652567842435bdcb2f894759b262f7b3
Ciprian Chesa <chesa_ciprian@yahoo.com>
2021-01-21 19:49:42 +0200

    improvements for hide all pages

--- functions.js
+++ functions.js
@@ -7,13 +7,15 @@ function show(id) {
 }
 
 function hideAllPages() {
-    var pagesIds = ["home", "skills", "languages", "projects"];
-    console.debug(pagesIds);
+
+    var pages = document.querySelectorAll(".page");
 
     //initial ; condition   ; post execution
-    for(var i = 0; i < pagesIds.length; i++) {
-        console.info('i = ', i);
-        hide(pagesIds[i]);
+    for(var i = 0; i < pages.length; i++) {
+        var page = pages[i];
+        hide(page.id);
+        // hide(pages[i].id);
+       // hide(pagesIds[i]);
     }
 }
 

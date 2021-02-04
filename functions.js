6898813c9fb0097684b7ace1876fbc4fc7106e0f
Ciprian Chesa <chesa_ciprian@yahoo.com>
2021-02-04 18:02:07 +0200

    use pageIds.forEach

--- functions.js
+++ functions.js
@@ -11,10 +11,9 @@ function hideAllPages() {
     var pageIds = pages.map(function(page) {
         return page.id;
     });
-
-    for(var i = 0; i < pageIds.length; i++) {
-        hide(pageIds[i]);
-    }
+    pageIds.forEach(function(pageId) {
+        hide(pageId);
+    })
 }
 
 function showPage(pageId) {

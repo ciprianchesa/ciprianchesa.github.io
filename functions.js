ad51713c1871ab9a63a43782d34f9dc3546dd25b
Ciprian Chesa <chesa_ciprian@yahoo.com>
2021-02-10 16:01:09 +0200

    improve hideAllPages using a single forEach

--- functions.js
+++ functions.js
@@ -8,12 +8,9 @@ function show(id) {
 
 function hideAllPages() {
     var pages = Array.from(document.querySelectorAll(".page"));
-    var pageIds = pages.map(function(page) {
-        return page.id;
+    pages.forEach(function(page) {
+        hide(page.id);
     });
-    pageIds.forEach(function(pageId) {
-        hide(pageId);
-    })
 }
 
 function showPage(pageId) {

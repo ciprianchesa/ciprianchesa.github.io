eb48cbfd688f3bb5191c5a33ab109ad810a1aa45
Ciprian Chesa <chesa_ciprian@yahoo.com>
2021-02-11 15:08:09 +0200

    replace allSkills from string to array

--- functions.js
+++ functions.js
@@ -30,11 +30,13 @@ function listenMenuClicks() {
 
 listenMenuClicks();
 
-var allSkills = `
-    <li class="favorite-skill">HTML</li>
-    <li>CSS</li>
-    <li class="favorite-skill">JS</li>
-`;
+showPage("skills");
+
+var allSkills = [
+    `<li class="favorite-skill">HTML</li>`,
+    `<li>CSS</li>`,
+    `<li class="favorite-skill">JS</li>`
+];
 
 var skillsEl = document.querySelector("#skills ul");
-skillsEl.innerHTML = allSkills;
+skillsEl.innerHTML = allSkills.join("");
